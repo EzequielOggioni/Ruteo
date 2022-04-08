1030bea3f17c78a278a547075d94ae024f10db12
Ezequiel <oggioni@providencia.com.ar>
2022-04-07 21:19:16 -0300

    ruteo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { JuegosComponent } from './page/juegos/juegos.component';
+import { LoginComponent } from './page/login/login.component';
+import { NotFoundComponent } from './page/not-found/not-found.component';
+import { PreguntadosComponent } from './page/preguntados/preguntados.component';
+import { TatetiComponent } from './page/tateti/tateti.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch:'full'  },
+  { path: 'login', component: LoginComponent },
+  {
+    path: 'juego', component: JuegosComponent, children: [
+      { path: 'preguntado', component:PreguntadosComponent },
+      { path: 'tateti', component: TatetiComponent }
+    ]
+  },
+  { path: '**', component: NotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './page/login/login.component';
+import { MenuComponent } from './page/menu/menu.component';
+import { JuegosComponent } from './page/juegos/juegos.component';
+import { TatetiComponent } from './page/tateti/tateti.component';
+import { PreguntadosComponent } from './page/preguntados/preguntados.component';
+import { NotFoundComponent } from './page/not-found/not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    MenuComponent,
+    JuegosComponent,
+    TatetiComponent,
+    PreguntadosComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
